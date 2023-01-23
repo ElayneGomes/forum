9894924f22d3bdd46a4a97e24f0af59263f054d0
ElayneGomes <elaynegomes@gmail.com>
2023-01-23 16:03:57 -0300

    SpringBoot e Teste: Profiles, Testes e Deploy(Evoluindo o código):
    - Voltando para o padrão .jar;

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,6 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -11,14 +9,10 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 @EnableSpringDataWebSupport
 @EnableCaching
 //@EnableSwagger2
-public class ForumApplication extends SpringBootServletInitializer {
+public class ForumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(ForumApplication.class);
-	}
 }
